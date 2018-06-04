9d9fc1e2415761d6c05b77b40506df73c8a4c1cd
Chevalier Tan Ga foo <chevaliertann@gmail.com>
2018-06-04 15:51:48 +0800

    Initial Commit

---
+++ GitTry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTry
+//
+//  Created by Tan Ga Foo on 04/06/2018.
+//  Copyright © 2018 Tan Ga Foo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
